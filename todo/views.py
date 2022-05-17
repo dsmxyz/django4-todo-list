2cd30f466c1059982e9da11266cf89c9dee534fb
dsmxyz <d.majarali@live.com>
2022-05-16 22:17:19 -0400

    tweaks

--- todo/views.py
+++ todo/views.py
@@ -8,42 +8,56 @@ from .models import Todo
 from django.utils import timezone
 from django.contrib.auth.decorators import login_required
 
-def home(request):
-    return render(request, 'todo/home.html')
 
-# Create your views here.
 def signupuser(request):
     if request.method == 'GET':
-        return render(request, 'todo/signupuser.html', {'form':UserCreationForm()})
+        return render(request, 'todo/signupuser.html', {'form': UserCreationForm()})
     else:
         if request.POST['password1'] == request.POST['password2']:
             try:
-                user = User.objects.create_user(request.POST['username'], password=request.POST['password1'])
+                user = User.objects.create_user(
+                    request.POST['username'], password=request.POST['password1'])
                 user.save()
                 login(request, user)
                 return redirect('currenttodos')
             except IntegrityError:
-                return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error': 'Username already exists.'})
+                return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error': 'Username already exists.'})
         else:
-            return render(request, 'todo/signupuser.html', {'form':UserCreationForm(), 'error': 'Passwords did not match.'})
+            return render(request, 'todo/signupuser.html', {'form': UserCreationForm(), 'error': 'Passwords did not match.'})
 
 
 def loginuser(request):
     if request.method == 'GET':
-        return render(request, 'todo/loginuser.html', {'form':AuthenticationForm()})
+        return render(request, 'todo/loginuser.html', {'form': AuthenticationForm()})
     else:
-        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        user = authenticate(
+            request, username=request.POST['username'], password=request.POST['password'])
         if user is None:
-             return render(request, 'todo/loginuser.html', {'form':AuthenticationForm(), 'error': 'Username or password does not match.'})
+            return render(request, 'todo/loginuser.html', {'form': AuthenticationForm(), 'error': 'Username or password does not match.'})
         else:
             login(request, user)
             return redirect('currenttodos')
-    
+
+
+def home(request):
+    return render(request, 'todo/home.html')
+
+
+def features(request):
+    return render(request, 'todo/features.html')
+
+
+@login_required
+def dashboard(request):
+    return render(request, 'todo/dashboard.html')
+
+
 @login_required
 def logoutuser(request):
     if request.method == 'POST':
         logout(request)
         return redirect('home')
+
 
 @login_required
 def createtodo(request):
@@ -59,10 +73,12 @@ def createtodo(request):
         except ValueError:
             return render(request, 'todo/createtodo.html', {'form': TodoForm(), 'error': 'Data can not be validated. Please try again.'})
 
+
 @login_required
 def currenttodos(request):
     todos = Todo.objects.filter(user=request.user, datecompleted__isnull=True)
     return render(request, 'todo/currenttodos.html', {'todos': todos})
+
 
 @login_required
 def viewtodo(request, todo_pk):
@@ -78,6 +94,7 @@ def viewtodo(request, todo_pk):
         except ValueError:
             return render(request, 'todo/viewtodo.html', {'todo': todo, 'form': form, 'error': 'Data was not able to be validated. Please try again.'})
 
+
 @login_required
 def completetodo(request, todo_pk):
     todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
@@ -86,20 +103,17 @@ def completetodo(request, todo_pk):
         todo.save()
         return redirect('currenttodos')
 
+
+@login_required
+def completedtodos(request):
+    todos = Todo.objects.filter(
+        user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
+    return render(request, 'todo/completedtodos.html', {'todos': todos})
+
+
 @login_required
 def deletetodo(request, todo_pk):
     todo = get_object_or_404(Todo, pk=todo_pk, user=request.user)
     if request.method == 'POST':
         todo.delete()
         return redirect('currenttodos')
-
-@login_required
-def completedtodos(request):
-    todos = Todo.objects.filter(user=request.user, datecompleted__isnull=False).order_by('-datecompleted')
-    return render(request, 'todo/completedtodos.html', {'todos': todos})
-
-def features(request):
-    return render(request, 'todo/features.html')
-
-def dashboard(request):
-    return render(request, 'todo/dashboard.html')
